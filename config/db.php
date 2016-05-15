6995e6ab1af751cd0e7802cedca9381cd2d2ad75
Sergey <snenko@gmail.com>
2016-05-15 15:53:55 +0300

    created database

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=test_blog',
     'username' => 'root',
-    'password' => '',
+    'password' => '1',
     'charset' => 'utf8',
 ];
